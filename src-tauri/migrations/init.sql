8535a4c8f127110e5725e544189ad9e203b92365
yujikawa <ykyujikawa@gmail.com>
2024-04-26 15:56:21 +0900

    Add task

---
+++ src-tauri/migrations/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY autoincrement
+    , title varchar(100)
+    , description text
+    , deadline date
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,100 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import Database from "tauri-plugin-sql-api";
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import Button from '@mui/material/Button';
+import { TextareaAutosize } from '@mui/base';
+import { Input } from '@mui/base/Input';
+
+function renderTable(result: Array<any>) {
+  const rows = result.map((row) => <tr>
+    <td>{row.title}</td>
+    <td>{row.description}</td>
+    <td>{row.deadline}</td>
+  </tr>)
+  return (
+    rows
+  )
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [deadline, setDeadline] = useState(new Date());
+  const [result, setResult] = useState([]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  useEffect(() => {
+    get_tasks()
+  }, [result]);
+
+
+  async function register_task() {
+    const db = await Database.load("sqlite:tasker.db");
+    const result = await db.execute(
+      "insert into task (title, description, deadline) values ($1, $2, $3)",
+      [title, description, deadline]
+    );
+    console.log(result.rowsAffected);
   }
+
+  async function get_tasks() {
+    const db = await Database.load("sqlite:tasker.db");
+    const result = await db.select(
+      "select id, title, description, deadline from task"
+    );
+    setResult(result);
+  }
+
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1>Tasker</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <p>Create your task</p>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
+      <form className="container"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          register_task();
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+        <Input
+          id="title-input"
+          className="input-group"
+          onChange={(e) => setTitle(e.currentTarget.value)}
+          placeholder="Enter title"
         />
-        <button type="submit">Greet</button>
+        <TextareaAutosize
+          id="description-input"
+          className="input-group"
+          minRows={3}
+          onChange={(e) => setDescription(e.currentTarget.value)}
+          placeholder="Enter description"
+        />
+        <DatePicker
+          className="input-group"
+          selected={deadline}
+          onChange={(date) => setDeadline(date)}
+        />
+        <Button type="submit"
+          >Register</Button>
       </form>
 
-      <p>{greetMsg}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>description</th>
+            <th>deadline</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderTable(result)}
+        </tbody>
+      </table>
     </div>
   );
 }
